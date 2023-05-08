387d176b83b1b0c9efa1a6c8c9fb38b01b718c0a
wanghao <wh@marsgis.cn>
2023-05-08 18:05:21 +0800

    同步代码 v3.5.6

--- example/layer-tile/online/google/map.js
+++ example/layer-tile/online/google/map.js
@@ -1,9 +1,9 @@
-import * as mars3d from "mars3d"
+// import * as mars3d from "mars3d"
 
-export let map // mars3d.Map三维地图对象
+var map // mars3d.Map三维地图对象
 
 // 需要覆盖config.json中地图属性参数（当前示例框架中自动处理合并）
-export const mapOptions = {
+var mapOptions = {
   scene: {
     center: { lat: 31.675177, lng: 117.323257, alt: 81193, heading: 0, pitch: -79 }
   },
@@ -43,7 +43,7 @@ export const mapOptions = {
   ]
 }
 
-export const eventTarget = new mars3d.BaseClass() // 事件对象，用于抛出事件到面板中
+var eventTarget = new mars3d.BaseClass() // 事件对象，用于抛出事件到面板中
 
 /**
  * 初始化地图业务，生命周期钩子函数（必须）
@@ -51,7 +51,7 @@ export const eventTarget = new mars3d.BaseClass() // 事件对象，用于抛出
  * @param {mars3d.Map} mapInstance 地图对象
  * @returns {void} 无
  */
-export function onMounted(mapInstance) {
+function onMounted(mapInstance) {
   map = mapInstance // 记录map
 
   // globalNotify("已知问题提示", `目前国家国家测绘主管部门对未经审核批准的谷歌等地图做了屏蔽封锁,目前谷歌地图服务暂不可用，需翻墙使用。`)
@@ -61,14 +61,14 @@ export function onMounted(mapInstance) {
  * 释放当前地图业务的生命周期函数
  * @returns {void} 无
  */
-export function onUnmounted() {
+function onUnmounted() {
   map = null
 }
 
 // 叠加的图层
 let tileLayer
 
-export function addTileLayer() {
+function addTileLayer() {
   removeTileLayer()
 
   // 方式2：在创建地球后调用addLayer添加图层(直接new对应type类型的图层类)
@@ -80,7 +80,7 @@ export function addTileLayer() {
   map.addLayer(tileLayer)
 }
 
-export function removeTileLayer() {
+function removeTileLayer() {
   if (tileLayer) {
     map.removeLayer(tileLayer, true)
     tileLayer = null
